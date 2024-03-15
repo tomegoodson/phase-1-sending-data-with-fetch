2f8c07ab2fb25ffd8da99fd9040cd6bdce0942e3
tomegoodson <158308016+tomegoodson@users.noreply.github.com>
2024-03-15 16:46:49 -0500

    fetch.md

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Add your code here
+function submitData(name, email) {
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    };
+    return fetch("http://localhost:3000/users", configurationObject)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            const body = document.querySelector('body');
+            body.innerHTML = `ID: ${data.id}`;
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            const body = document.querySelector('body');
+            body.innerHTML = `Error: ${error.message}`;
+        });
+}
+
+document.querySelector("form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    submitData(e.target.name.value, e.target.email.value);
+});
